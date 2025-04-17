3de5f3e9c2a3a077f773a5eac1aa18016eab9199
fahmi firmansyah <fahmifirmansyah194@gmail.com>
2025-04-17 09:57:31 +0700

    membuat website kode diksi

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+function aosInit() {
+  AOS.init({
+    // duration: 600,
+    // easing: "ease-in-out",
+    // once: true,
+    // mirror: false,
+  });
+}
+window.addEventListener("load", aosInit);
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+const closeBtn = document.getElementById("closeBtn");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.add("active");
+  hamburger.classList.add("hide");
+});
+
+closeBtn.addEventListener("click", () => {
+  navMenu.classList.remove("active");
+  hamburger.classList.remove("hide"); // Tampilkan lagi hamburger
+});
+
+const portdisplay = document.getElementById("portdisplay");
+const btnDown = document.getElementById("btnDown");
+const btnUp = document.getElementById("btnUp");
+
+btnDown.addEventListener("click", () => {
+  portdisplay.classList.add("active");
+  btnUp.classList.add("active");
+  btnDown.classList.add("hide");
+
+  btnUp.addEventListener("click", () => {
+    portdisplay.classList.remove("active");
+    btnDown.classList.remove("hide");
+    btnUp.classList.remove("active");
+  });
+});
+
+function initSwiper() {
+  document.querySelectorAll(".init-swiper").forEach(function (swiperElement) {
+    let config = JSON.parse(swiperElement.querySelector(".swiper-config").innerHTML.trim());
+
+    if (swiperElement.classList.contains("swiper-tab")) {
+      initSwiperWithCustomPagination(swiperElement, config);
+    } else {
+      new Swiper(swiperElement, config);
+    }
+  });
+}
+window.addEventListener("load", initSwiper);
